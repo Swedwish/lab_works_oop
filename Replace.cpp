2cf041025c8ead984d3b69aadf86fe934788387e
Swedwish <swedwish>
2021-12-12 22:23:39 +0700

    Finished lab2.

--- Replace.cpp
+++ Replace.cpp
@@ -12,6 +12,8 @@ Replace::Replace(const string& word1, const string& word2) {
 bool Replace::exec(vector<string> *input, vector<string> *output) {
     int index;
     output->clear();
+    if (args[1].find(args[0])!= string::npos)
+        Validator::badFile("In \"replace\" first string can't be substring of the second.");
     for (auto& i: *input){
         while ((index = i.find(args[0]))!=string::npos){
             i.replace(index,args[0].length(),args[1]);
