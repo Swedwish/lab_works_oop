08a69e7bab77732d1255567d6cd8e47621ac4e2b
Swedwish <swedwish>
2021-12-19 21:27:41 +0700

    .

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -2,36 +2,25 @@
 #include <tuple>
 #include <functional>
 #include <iostream>
+#include <iosfwd>
+#include <vector>
+#include <fstream>
+
 using namespace std;
-
-template<const int index, typename ...Args>
-class iterate {
-public:
-    static void next(tuple<Args...> &Tuple, ostream &stream) {
-        iterate<index-1, Args...>::next(Tuple, stream);
-        stream<< get<index-1>(Tuple)<<" ";
-    }
-};
-
-template<typename Ch, typename Tr, typename ... Args>
-auto &operator<<(std::basic_ostream<Ch, Tr> &os, std::tuple<Args...> &t)
-{
-    int const length = tuple_size<tuple<Args...>>::value;
-    iterate<length,Args...>::next(t,os);
-    return os;
-}
-
-
-
-template<typename ...Args>
-class iterate<0, Args...> {
-public:
-    static void next(tuple<Args...> &Tuple, ostream& stream) {
-        //stream<<get<0>(Tuple)<<" ";
-    }
-};
+#include "print_tuple.h"
+#include "CSVparser.h"
 
 int main(){
-    tuple<int, string,string,string> t = {3, "Hello","World", "!!!"};
-    cout<<t<<endl;
+    std::ifstream f("test.csv");
+    CSVParser::CSVParser<std::string, int, float> parser(f, 0);
+    std::cout << "Started reading" << std::endl;
+    try {
+        for (const auto& entry: parser) {
+            cout << entry << std::endl;
+        }
+    } catch (CSVParser::ParsingException& e) {
+        std::cout << e.what() << "; line: " << e.get_line_idx() << " char: " << e.get_char_idx() << std::endl;
+    }
+    std::cout << "Read all" << std::endl;
+    return 0;
 }
